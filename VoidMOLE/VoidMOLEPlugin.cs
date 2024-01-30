53833e6cbf71ca78531212dd691d94f507df6398
CubeSuite <cubesuite@outlook.com>
2024-01-30 20:36:48 +0000

    Add project files.

---
+++ VoidMOLE/VoidMOLEPlugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace VoidMOLE
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class VoidMOLEPlugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.equinox.VoidMOLE";
+        private const string PluginName = "VoidMOLE";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        private void Awake() {
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.PatchAll();
+
+            // ToDo: Apply Patches
+
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+            Log = Logger;
+        }
+
+        private void Update() {
+            // ToDo: Delete If Not Needed
+        }
+    }
+}
